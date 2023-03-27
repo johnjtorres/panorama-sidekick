7c15dd2808a0305b3ef8f66a8a638f8cbe9e6013
John Torres <johnnyjamestorres@outlook.com>
2023-03-27 16:47:31 -0700

    Implement device_groups show function

--- panorama_sidekick/show.py
+++ panorama_sidekick/show.py
@@ -1,8 +1,17 @@
 """Defines the commands for showing lists of things."""
 
+import xml.etree.ElementTree as ET
+
 import click
 
 
 @click.command()
-def device_groups():
-    ...
+@click.pass_obj
+def device_groups(panorama):
+    cmd = "<show><devicegroups/></show>"
+    show_devicegroups = panorama.op(cmd)
+    tree = ET.fromstring(show_devicegroups)
+    device_group_names = "\n".join(
+        dg.attrib["name"] for dg in tree.findall(".//devicegroups/entry")
+    )
+    click.echo(device_group_names)
