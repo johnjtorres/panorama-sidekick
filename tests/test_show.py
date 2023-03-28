0e327a79b58cef8eb9e7a8197dead24ec0158d5f
John Torres <johnnyjamestorres@outlook.com>
2023-03-27 18:36:00 -0700

    Separate test data to testing directory

--- tests/test_show.py
+++ tests/test_show.py
@@ -2,22 +2,12 @@
 
 from panorama_sidekick.cli import cli
 from panorama_sidekick.panorama import Panorama
+from testing.utils import read_test_data_file
 
 
 def test_pano_show_device_groups(runner, monkeypatch):
     """Test for listing device groups."""
-    test_xml = """\
-    <response status="success">
-      <result>
-        <devicegroups>
-          <entry name="DG1"></entry>
-          <entry name="DG2"></entry>
-          <entry name="DG3"></entry>
-        </devicegroups>
-      </result>
-    </response>
-    """
-
+    test_xml = read_test_data_file("show_devicegroups.xml")
     monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
     result = runner.invoke(cli, ["show", "device-groups"])
     assert "DG1\nDG2\nDG3\n" == result.output
