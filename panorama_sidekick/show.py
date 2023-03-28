63fcfe0d77a51d48e51a960b129a9ed25391abf1
John Torres <johnnyjamestorres@outlook.com>
2023-03-27 17:13:05 -0700

    Add type annotations, rename operational to operational_command

--- panorama_sidekick/show.py
+++ panorama_sidekick/show.py
@@ -4,12 +4,19 @@ import xml.etree.ElementTree as ET
 
 import click
 
+from panorama_sidekick.panorama import Panorama
+
 
 @click.command()
 @click.pass_obj
-def device_groups(panorama):
+def device_groups(panorama: Panorama) -> None:
+    """Print a list of device groups.
+
+    Args:
+        panorama (Panorama): Panorama object
+    """
     cmd = "<show><devicegroups/></show>"
-    show_devicegroups = panorama.operational(cmd)
+    show_devicegroups = panorama.operational_command(cmd)
     tree = ET.fromstring(show_devicegroups)
     device_group_names = "\n".join(
         dg.attrib["name"] for dg in tree.findall(".//devicegroups/entry")
