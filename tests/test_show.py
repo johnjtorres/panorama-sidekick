7548cc0edb74f9bfca0ef236c7d61a76e94560ef
John Torres <johnnyjamestorres@outlook.com>
2023-03-28 14:33:20 -0700

    Add tests for filtering pano show firewalls by device group

--- tests/test_show.py
+++ tests/test_show.py
@@ -9,7 +9,6 @@ from testing.utils import read_test_data_file
 
 
 def test_pano_show_device_groups(runner: CliRunner, monkeypatch: MonkeyPatch) -> None:
-    """Test for listing device groups."""
     test_xml = read_test_data_file("show_devicegroups.xml")
     monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
     result = runner.invoke(cli, ["show", "device-groups"])
@@ -17,8 +16,26 @@ def test_pano_show_device_groups(runner: CliRunner, monkeypatch: MonkeyPatch) ->
 
 
 def test_pano_show_firewalls(runner: CliRunner, monkeypatch: MonkeyPatch) -> None:
-    """Test for listing firewalls."""
     test_xml = read_test_data_file("show_devicegroups.xml")
     monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
     result = runner.invoke(cli, ["show", "firewalls"])
     assert "FW1\nFW2\nFW3\nFW4\nFW5\nFW6\n" == result.output
+
+
+def test_pano_show_firewalls_filter_on_devicegroup_that_exists(
+    runner: CliRunner, monkeypatch: MonkeyPatch
+) -> None:
+    test_xml = read_test_data_file("show_devicegroups.xml")
+    monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
+    result = runner.invoke(cli, ["show", "firewalls", "--device-group", "DG1"])
+    assert "FW1\nFW2\n" == result.output
+
+
+def test_pano_show_firewalls_filter_on_devicegroup_that_does_not_exist(
+    runner: CliRunner, monkeypatch: MonkeyPatch
+) -> None:
+    test_xml = read_test_data_file("show_devicegroups.xml")
+    monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
+    result = runner.invoke(cli, ["show", "firewalls", "--device-group", "DG0"])
+    assert "" == result.output
+    assert 0 == result.exit_code
