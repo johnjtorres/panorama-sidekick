e098478526e78be0b5903cb1b74d4cd0c0a7a2a7
John Torres <johnnyjamestorres@outlook.com>
2023-03-28 09:30:55 -0700

    Add test for pano show firewalls

--- tests/test_show.py
+++ tests/test_show.py
@@ -14,3 +14,11 @@ def test_pano_show_device_groups(runner: CliRunner, monkeypatch: MonkeyPatch) ->
     monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
     result = runner.invoke(cli, ["show", "device-groups"])
     assert "DG1\nDG2\nDG3\n" == result.output
+
+
+def test_pano_show_firewalls(runner: CliRunner, monkeypatch: MonkeyPatch) -> None:
+    """Test for listing firewalls."""
+    test_xml = read_test_data_file("show_devicegroups.xml")
+    monkeypatch.setattr(Panorama, "get", lambda *args, **kwargs: test_xml)
+    result = runner.invoke(cli, ["show", "firewalls"])
+    assert "FW1\nFW2\nFW3\nFW4\nFW5\nFW6\n" == result.output
